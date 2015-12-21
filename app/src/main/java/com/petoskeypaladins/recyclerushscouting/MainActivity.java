055508bde59fffa76e418e970a3c70c66a6d50b7
Brendon-Egolf <begolf123@gmail.com>
2015-12-21 13:17:12 -0500

    Initial commit

---
+++ app/src/main/java/com/petoskeypaladins/recyclerushscouting/MainActivity.java
@@ -0,0 +1,42 @@
+package com.petoskeypaladins.recyclerushscouting;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ScoutingSheet(), "Scout");
+        adapter.addFragment(new DataView(), "View Data");
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
