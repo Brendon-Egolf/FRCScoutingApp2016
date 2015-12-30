279dc52478f4e0b30a889909c8c3a2e5cd85c55f
Brendon-Egolf <begolf123@gmail.com>
2015-12-30 14:54:07 -0500

    adding scouting sheet layout

---
+++ app/src/main/java/com/petoskeypaladins/recyclerushscouting/Stack.java
@@ -0,0 +1,34 @@
+package com.petoskeypaladins.recyclerushscouting;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Stack extends Fragment {
+
+    public Stack() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stack, container, false);
+    }
+
+
+
+
+
+}
