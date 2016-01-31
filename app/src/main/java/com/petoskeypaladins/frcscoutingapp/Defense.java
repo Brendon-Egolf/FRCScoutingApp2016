507525a600133f63f9f57c8cbb949c5c8b1adaed
Brendon-Egolf <begolf123@gmail.com>
2016-01-30 20:15:50 -0500

    I finished the layout!

--- app/src/main/java/com/petoskeypaladins/frcscoutingapp/Defense.java
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/Defense.java
@@ -4,37 +4,67 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Defense extends LinearLayout {
+    private int defensePasses;
 
-
-    public Defense(Context context) {
+    public Defense(Context context, Boolean lowBar) {
         super(context);
 
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.layout_defense, this, true);
         View view = inflater.inflate(R.layout.layout_defense, this, true);
 
-        view.findViewById(R.id.defense_type);
-
-
         final Spinner defenseSpinner = (Spinner) view.findViewById(R.id.defense_type);
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.defenses, android.R.layout.simple_spinner_dropdown_item);
+        final Button subtractPass = (Button) view.findViewById(R.id.subtract_pass);
+        final TextView passes = (TextView) view.findViewById(R.id.passes);
+        final Button addPass = (Button) view.findViewById(R.id.add_pass);
 
+        ArrayAdapter<CharSequence> arrayAdapter;
+        defensePasses = 0;
+        updatePasses(passes, defensePasses);
+        if (lowBar) {
+            Toast.makeText(context, "shorty got low low low low low low low low.", Toast.LENGTH_SHORT).show();
+            arrayAdapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.low_bar, android.R.layout.simple_spinner_dropdown_item);
+        } else {
+            arrayAdapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.teleop_defenses, android.R.layout.simple_spinner_dropdown_item);
+        }
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         defenseSpinner.setAdapter(arrayAdapter);
 
+        subtractPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses--;
+                updatePasses(passes, defensePasses);
+                if (defensePasses == 0) {
+                    subtractPass.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
-
-
-
+        addPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses++;
+                updatePasses(passes, defensePasses);
+                if (defensePasses > 0) {
+                    subtractPass.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
-
+    public void updatePasses(TextView passes, int defensePasses) {
+        passes.setText(Integer.toString(defensePasses));
+    }
 
 }
--- app/src/main/java/com/petoskeypaladins/frcscoutingapp/ScoutingSheet.java
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/ScoutingSheet.java
@@ -19,7 +19,7 @@ import android.widget.Toast;
 
 
 public class ScoutingSheet extends Fragment {
-
+    private int highGoalCount, lowGoalCount;
 
 
 
@@ -49,7 +49,7 @@ public class ScoutingSheet extends Fragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                submitForm();
+                submitForm(view);
             }
         });
 
@@ -58,23 +58,14 @@ public class ScoutingSheet extends Fragment {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    roundNumber.clearFocus();
+                    teamNumber.requestFocus();
+
                 }
 
                 return false;
             }
         });
 
-        teamNumber.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    teamNumber.clearFocus();
-                }
-
-                return false;
-            }
-        });
 
         final Spinner autonDefenseSpinner = (Spinner) view.findViewById(R.id.auton_defense_cross);
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(),
@@ -125,8 +116,67 @@ public class ScoutingSheet extends Fragment {
         final int DEFENSES = 5;
 
         for (int i = 0; i < DEFENSES; i++) {
-            defenseList.addView(new Defense(getContext()));
+            defenseList.addView(new Defense(getContext(), (i == 0)));
         }
+
+        final Button highGoalAdd = (Button) view.findViewById(R.id.high_goal_add);
+        final TextView highGoalNumber = (TextView) view.findViewById(R.id.high_goal_number);
+        final Button highGoalSubtract = (Button) view.findViewById(R.id.high_goal_subtract);
+        final Button lowGoalAdd = (Button) view.findViewById(R.id.low_goal_add);
+        final TextView lowGoalNumber = (TextView) view.findViewById(R.id.low_goal_number);
+        final Button lowGoalSubtract = (Button) view.findViewById(R.id.low_goal_subtract);
+
+        highGoalCount = 0;
+        highGoalNumber.setText(Integer.toString(highGoalCount));
+        lowGoalCount = 0;
+        lowGoalNumber.setText(Integer.toString(lowGoalCount));
+
+
+        highGoalSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                highGoalCount--;
+                highGoalNumber.setText(Integer.toString(highGoalCount));
+                if (highGoalCount == 0) {
+                    highGoalSubtract.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        highGoalAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                highGoalCount++;
+                highGoalNumber.setText(Integer.toString(highGoalCount));
+                if (highGoalCount > 0) {
+                    highGoalSubtract.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        lowGoalSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lowGoalCount--;
+                lowGoalNumber.setText(Integer.toString(lowGoalCount));
+                if (lowGoalCount == 0) {
+                    lowGoalSubtract.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        lowGoalAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lowGoalCount++;
+                lowGoalNumber.setText(Integer.toString(lowGoalCount));
+                if (lowGoalCount > 0) {
+                    lowGoalSubtract.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
 
 
 
@@ -136,19 +186,11 @@ public class ScoutingSheet extends Fragment {
 
 
 
-    public void submitForm() {
-
-        EditText roundNumber = (EditText)getView().findViewById(R.id.round_number);
-        EditText teamNumber = (EditText) getView().findViewById(R.id.team_number);
-        CheckBox canAuton = (CheckBox) getView().findViewById(R.id.can_auton);
-
-
+    public void submitForm(View view) {
 
     }
 
-    public void writeData(View view) {
 
-    }
 
 
     public void alert() {
