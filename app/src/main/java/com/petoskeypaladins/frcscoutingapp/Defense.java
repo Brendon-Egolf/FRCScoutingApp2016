b3914ed32b5026705b0f6c38e663253e0550e05c
Brendon-Egolf <begolf123@gmail.com>
2016-02-07 20:30:31 -0500

    The output is done now onto analysis.

--- app/src/main/java/com/petoskeypaladins/frcscoutingapp/Defense.java
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/Defense.java
@@ -1,38 +1,59 @@
 package com.petoskeypaladins.frcscoutingapp;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 
-public class Defense extends LinearLayout {
+public class Defense extends ViewGroup {
     private int defensePasses;
 
-    public Defense(Context context, Boolean lowBar) {
+    public Defense(Context context) {
         super(context);
+        init(context, null);
+    }
+
+    public Defense(Context context, AttributeSet attributeSet) {
+        super(context);
+        init(context, attributeSet);
+    }
+
+    public Defense(Context context, AttributeSet attributeSet, int defStyle) {
+        super(context);
+        init(context, attributeSet);
+    }
+
+    public void init(Context context, AttributeSet attributeSet) {
 
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.layout_defense, this, true);
-        View view = inflater.inflate(R.layout.layout_defense, this, true);
+        View view = inflater.inflate(R.layout.layout_defense, null);
 
         final Spinner defenseSpinner = (Spinner) view.findViewById(R.id.defense_type);
         final Button subtractPass = (Button) view.findViewById(R.id.subtract_pass);
         final TextView passes = (TextView) view.findViewById(R.id.passes);
         final Button addPass = (Button) view.findViewById(R.id.add_pass);
 
+        TypedArray attributes = context.obtainStyledAttributes(R.styleable.Defense);
+        boolean lowBar = Boolean.parseBoolean(attributes.getString(R.styleable.Defense_isLowBar));
+
+
+
         ArrayAdapter<CharSequence> arrayAdapter;
         defensePasses = 0;
         updatePasses(passes, defensePasses);
         if (lowBar) {
-            arrayAdapter = ArrayAdapter.createFromResource(getContext(),
+            arrayAdapter = ArrayAdapter.createFromResource(context,
                     R.array.low_bar, android.R.layout.simple_spinner_dropdown_item);
         } else {
-            arrayAdapter = ArrayAdapter.createFromResource(getContext(),
+            arrayAdapter = ArrayAdapter.createFromResource(context,
                     R.array.teleop_defenses, android.R.layout.simple_spinner_dropdown_item);
         }
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -59,6 +80,16 @@ public class Defense extends LinearLayout {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
     }
 
     public void updatePasses(TextView passes, int defensePasses) {
--- app/src/main/java/com/petoskeypaladins/frcscoutingapp/ScoutingSheet.java
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/ScoutingSheet.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -24,9 +25,9 @@ import java.io.FileWriter;
 
 public class ScoutingSheet extends Fragment {
     private int highGoalMadeCount, highGoalMissedCount, lowGoalMadeCount, lowGoalMissedCount;
-
-
-
+    private int[] defensePasses = new int[5];
+    BufferedWriter writer;
+    final int ONE = 1;
 
     public ScoutingSheet() {
         // Required empty public constructor
@@ -57,7 +58,6 @@ public class ScoutingSheet extends Fragment {
             }
         });
 
-
         roundNumber.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -69,22 +69,6 @@ public class ScoutingSheet extends Fragment {
                 return false;
             }
         });
-
-
-//        final ScrollView scroll = (ScrollView)view.findViewById(R.id.scrollView);
-//        scroll.setOnTouchListener(new View.OnTouchListener() {
-//
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if (roundNumber.hasFocus()) {
-//                    roundNumber.clearFocus();
-//                }
-//                if (teamNumber.hasFocus()) {
-//                    teamNumber.clearFocus();
-//                }
-//                return false;
-//            }
-//        });
 
         final Spinner autonDefenseSpinner = (Spinner) view.findViewById(R.id.auton_defense);
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(),
@@ -127,16 +111,6 @@ public class ScoutingSheet extends Fragment {
                 }
             }
         });
-
-
-        final LinearLayout defenseList = (LinearLayout)view.findViewById(R.id.defense_list);
-        final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
-                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        final int DEFENSES = 5;
-
-        for (int i = 0; i < DEFENSES; i++) {
-            defenseList.addView(new Defense(getContext(), (i == 0)));
-        }
 
         final CheckBox canCapture = (CheckBox) view.findViewById(R.id.can_capture);
         final CheckBox canClimb = (CheckBox) view.findViewById(R.id.can_climb);
@@ -264,6 +238,172 @@ public class ScoutingSheet extends Fragment {
             }
         });
 
+        final Spinner defense1Type = (Spinner) view.findViewById(R.id.defense_1_type);
+        final Button defense1AddPass = (Button) view.findViewById(R.id.defense_1_add_pass);
+        final TextView defense1Passes = (TextView) view.findViewById(R.id.defense_1_passes);
+        final Button defense1SubtractPass = (Button) view.findViewById(R.id.defense_1_subtract_pass);
+
+        final Spinner defense2Type = (Spinner) view.findViewById(R.id.defense_2_type);
+        final Button defense2AddPass = (Button) view.findViewById(R.id.defense_2_add_pass);
+        final TextView defense2Passes = (TextView) view.findViewById(R.id.defense_2_passes);
+        final Button defense2SubtractPass = (Button) view.findViewById(R.id.defense_2_subtract_pass);
+
+        final Spinner defense3Type = (Spinner) view.findViewById(R.id.defense_3_type);
+        final Button defense3AddPass = (Button) view.findViewById(R.id.defense_3_add_pass);
+        final TextView defense3Passes = (TextView) view.findViewById(R.id.defense_3_passes);
+        final Button defense3SubtractPass = (Button) view.findViewById(R.id.defense_3_subtract_pass);
+
+        final Spinner defense4Type = (Spinner) view.findViewById(R.id.defense_4_type);
+        final Button defense4AddPass = (Button) view.findViewById(R.id.defense_4_add_pass);
+        final TextView defense4Passes = (TextView) view.findViewById(R.id.defense_4_passes);
+        final Button defense4SubtractPass = (Button) view.findViewById(R.id.defense_4_subtract_pass);
+
+        final Spinner defense5Type = (Spinner) view.findViewById(R.id.defense_5_type);
+        final Button defense5AddPass = (Button) view.findViewById(R.id.defense_5_add_pass);
+        final TextView defense5Passes = (TextView) view.findViewById(R.id.defense_5_passes);
+        final Button defense5SubtractPass = (Button) view.findViewById(R.id.defense_5_subtract_pass);
+
+        for (int i:defensePasses) {
+            i = 0;
+        }
+
+
+        defense1Passes.setText(Integer.toString(defensePasses[1-ONE]));
+        defense2Passes.setText(Integer.toString(defensePasses[2-ONE]));
+        defense3Passes.setText(Integer.toString(defensePasses[3-ONE]));
+        defense4Passes.setText(Integer.toString(defensePasses[4-ONE]));
+        defense5Passes.setText(Integer.toString(defensePasses[5-ONE]));
+
+        ArrayAdapter<CharSequence> defenseArrayAdapter;
+        ArrayAdapter<CharSequence> lowBarArrayAdapter;
+
+        lowBarArrayAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.low_bar, android.R.layout.simple_spinner_dropdown_item);
+        defenseArrayAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.teleop_defenses, android.R.layout.simple_spinner_dropdown_item);
+
+        defenseArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lowBarArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        defense1Type.setAdapter(lowBarArrayAdapter);
+
+        defense1SubtractPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[1 - ONE]--;
+                defense1Passes.setText(Integer.toString(defensePasses[1 - ONE]));
+                if (defensePasses[1 - ONE] == 0) {
+                    defense1SubtractPass.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        defense1AddPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[1 - ONE]++;
+                defense1Passes.setText(Integer.toString(defensePasses[1 - ONE]));
+                if (defensePasses[1 - ONE] > 0) {
+                    defense1SubtractPass.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        defense2Type.setAdapter(defenseArrayAdapter);
+        defense2SubtractPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[2 - ONE]--;
+                defense2Passes.setText(Integer.toString(defensePasses[2 - ONE]));
+                if (defensePasses[2 - ONE] == 0) {
+                    defense2SubtractPass.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        defense2AddPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[2 - ONE]++;
+                defense2Passes.setText(Integer.toString(defensePasses[2 - ONE]));
+                if (defensePasses[2 - ONE] > 0) {
+                    defense2SubtractPass.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        defense3Type.setAdapter(defenseArrayAdapter);
+        defense3SubtractPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[3 - ONE]--;
+                defense3Passes.setText(Integer.toString(defensePasses[3 - ONE]));
+                if (defensePasses[3 - ONE] == 0) {
+                    defense3SubtractPass.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        defense3AddPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[3 - ONE]++;
+                defense3Passes.setText(Integer.toString(defensePasses[3 - ONE]));
+                if (defensePasses[3 - ONE] > 0) {
+                    defense3SubtractPass.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        defense4Type.setAdapter(defenseArrayAdapter);
+        defense4SubtractPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[4 - ONE]--;
+                defense4Passes.setText(Integer.toString(defensePasses[4 - ONE]));
+                if (defensePasses[4 - ONE] == 0) {
+                    defense4SubtractPass.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        defense4AddPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[4 - ONE]++;
+                defense4Passes.setText(Integer.toString(defensePasses[4 - ONE]));
+                if (defensePasses[4 - ONE] > 0) {
+                    defense4SubtractPass.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        defense5Type.setAdapter(defenseArrayAdapter);
+        defense5SubtractPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[5 - ONE]--;
+                defense5Passes.setText(Integer.toString(defensePasses[5 - ONE]));
+                if (defensePasses[5 - ONE] == 0) {
+                    defense5SubtractPass.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        defense5AddPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defensePasses[5 - ONE]++;
+                defense5Passes.setText(Integer.toString(defensePasses[5 - ONE]));
+                if (defensePasses[5 - ONE] > 0) {
+                    defense5SubtractPass.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         return view;
     }
@@ -279,23 +419,101 @@ public class ScoutingSheet extends Fragment {
         CheckBox canAutonShoot = (CheckBox) view.findViewById(R.id.can_auton_shoot);
         RadioGroup autonShootType = (RadioGroup) view.findViewById(R.id.auton_shoot_type);
         LinearLayout teleopDefenses = (LinearLayout) view.findViewById(R.id.defense_list);
+        CheckBox capture = (CheckBox) view.findViewById(R.id.can_capture);
+        CheckBox climb = (CheckBox) view.findViewById(R.id.can_climb);
+        Spinner defense1Type = (Spinner) view.findViewById(R.id.defense_1_type);
+        Spinner defense2Type = (Spinner) view.findViewById(R.id.defense_2_type);
+        Spinner defense3Type = (Spinner) view.findViewById(R.id.defense_3_type);
+        Spinner defense4Type = (Spinner) view.findViewById(R.id.defense_4_type);
+        Spinner defense5Type = (Spinner) view.findViewById(R.id.defense_5_type);
+
+        if (roundNumber.getText().toString().equals("") || teamNumber.getText().toString().equals("")) {
+            Toast.makeText(getContext(), "Fill out the team and round number!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (defense2Type.getSelectedItem().toString().equals("Defense crossed?")) {
+            Toast.makeText(getContext(), "Fill out all defenses, 'defense crossed?' is not a defense", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (defense3Type.getSelectedItem().toString().equals("Defense crossed?")) {
+            Toast.makeText(getContext(), "Fill out all defenses, 'defense crossed?' is not a defense", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (defense4Type.getSelectedItem().toString().equals("Defense crossed?")) {
+            Toast.makeText(getContext(), "Fill out all defenses, 'defense crossed?' is not a defense", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (defense5Type.getSelectedItem().toString().equals("Defense crossed?")) {
+            Toast.makeText(getContext(), "Fill out all defenses, 'defense crossed?' is not a defense", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
 
 
         try {
             String fileName = teamNumber.getText().toString();
             File file = new File("/storage/emulated/0/scouting/" + fileName + ".csv");
+            new File("/storage/emulated/0/scouting/").mkdir();
+
+
             if (!file.exists()) {
                 file.createNewFile();
+                Toast.makeText(getContext(), "rise my son." + fileName, Toast.LENGTH_SHORT).show();
             }
-            BufferedWriter bufferedWriter;
-            bufferedWriter = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+
+            writer = new BufferedWriter(new FileWriter(file.getAbsoluteFile(), true));
+
+            write(roundNumber.getText().toString());
+            write(Boolean.toString(canAuton.isChecked()));
+            write(autonDefense.getSelectedItem().toString());
+            write(Boolean.toString(canAutonShoot.isChecked()));
+            if (canAutonShoot.isChecked()) {
+                RadioButton shootType = (RadioButton) view.findViewById(autonShootType.getCheckedRadioButtonId());
+                write(shootType.getText().toString());
+            } else {
+                write("null");
+            }
+            write(defense1Type.getSelectedItem().toString());
+            write(Integer.toString(defensePasses[1 - ONE]));
+            write(defense2Type.getSelectedItem().toString());
+            write(Integer.toString(defensePasses[2 - ONE]));
+            write(defense3Type.getSelectedItem().toString());
+            write(Integer.toString(defensePasses[3 - ONE]));
+            write(defense4Type.getSelectedItem().toString());
+            write(Integer.toString(defensePasses[4 - ONE]));
+            write(defense5Type.getSelectedItem().toString());
+            write(Integer.toString(defensePasses[5 - ONE]));
+            if ((highGoalMadeCount + highGoalMissedCount) > 0) {
+                write(Double.toString((double) highGoalMadeCount / (highGoalMadeCount + highGoalMissedCount)));
+            } else {
+                write("0.0");
+            }
+            if ((lowGoalMadeCount + lowGoalMissedCount) > 0) {
+                write(Double.toString((double) lowGoalMadeCount / (lowGoalMadeCount + lowGoalMissedCount)));
+            } else {
+                write("0.0");
+            }
+            write(Boolean.toString(capture.isChecked()));
+            writer.write(Boolean.toString(climb.isChecked()));
+            writer.newLine();
+            writer.flush();
 
 
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(getContext(), "IO error", Toast.LENGTH_SHORT).show();
         }
+
+
+
+
+        Toast.makeText(getContext(), "You have submitted your form to the scouting god.", Toast.LENGTH_SHORT).show();
+
+
     }
 
 
@@ -306,7 +524,14 @@ public class ScoutingSheet extends Fragment {
         toast.show();
     }
 
-
+    public void write(String data) {
+        try {
+            writer.write(data + ",");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "IO error", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 }
