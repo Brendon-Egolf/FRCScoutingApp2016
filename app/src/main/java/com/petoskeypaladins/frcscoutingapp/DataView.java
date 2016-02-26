3b75fa5c7bd05c83f25089d369c9200cbcc2bf8f
begolf <begolf123@gmail.com>
2016-02-25 20:38:17 -0500

    all the teams are showing up.

--- app/src/main/java/com/petoskeypaladins/frcscoutingapp/DataView.java
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/DataView.java
@@ -5,10 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DataView extends Fragment {
+    JSONArray teamJSON;
 
     public DataView() {
         // Required empty public constructor
@@ -20,10 +31,40 @@ public class DataView extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_view, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_data_view, container, false);
+        ArrayList<JSONObject> teamList = new ArrayList<>();
+        try {
+            teamJSON = new JSONArray(loadJSONFromAsset());
+            for (int i = 0; i < teamJSON.length(); i++) {
+                teamList.add((JSONObject) teamJSON.get(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
+        ListView listView = (ListView) view.findViewById(R.id.list);
+        listView.setAdapter(new TeamArrayAdapter(getContext(),
+                android.R.layout.simple_list_item_1,
+                R.layout.team_list_view,
+                teamList));
+
+        return view;
+    }
+
+    public String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream inputStream = getContext().getAssets().open("FRC-standish-event.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
---
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/TeamArrayAdapter.java
@@ -0,0 +1,51 @@
+package com.petoskeypaladins.frcscoutingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by begolf on 2/25/16.
+ */
+public class TeamArrayAdapter extends ArrayAdapter<JSONObject> {
+    private ArrayList<JSONObject> teamList;
+
+    public TeamArrayAdapter(Context context, int resource, int resourceId, ArrayList<JSONObject> teamList) {
+        super(context, resource, resourceId, teamList);
+        this.teamList = teamList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.team_list_view, parent, false);
+        TextView teamNumber = (TextView) view.findViewById(R.id.team_number);
+        TextView teamName = (TextView) view.findViewById(R.id.team_name);
+        TextView teamLocation = (TextView) view.findViewById(R.id.team_location);
+        try {
+            teamNumber.setText(Integer.toString(teamList.get(position).getInt("team_number")));
+            teamName.setText(teamList.get(position).getString("nickname"));
+            teamLocation.setText(teamList.get(position).getString("location"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+
+    @Override
+    public int getCount(){
+        return teamList.size();
+    }
+}
