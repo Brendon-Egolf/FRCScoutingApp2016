0cd16e3c71505ac3d17665e500cfff5d70a46707
begolf <begolf123@gmail.com>
2016-02-29 22:08:31 -0500

    created bluetooth

--- app/src/main/java/com/petoskeypaladins/frcscoutingapp/Bluetooth.java
+++ app/src/main/java/com/petoskeypaladins/frcscoutingapp/Bluetooth.java
@@ -2,37 +2,61 @@ package com.petoskeypaladins.frcscoutingapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothServerSocket;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
+import android.bluetooth.BluetoothSocket;
+import android.os.ParcelUuid;
+import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
-
-import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
 /**
  * Created by begolf on 2/29/16.
  */
 public class Bluetooth {
-    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    BluetoothServerSocket serverSocket;
-    ArrayAdapter<String> pairedDeviceNames;
+    private OutputStream outputStream;
+    private InputStream inStream;
 
-    public Bluetooth() {
-        //test for bluetooth called earlier
+    private Bluetooth() throws IOException{
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter != null) {
+            if (bluetoothAdapter.isEnabled()) {
+                Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
 
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                pairedDeviceNames.add(device.getName() + "\n" + device.getAddress());
+                if(bondedDevices.size() > 0){
+                    BluetoothDevice[] devices = (BluetoothDevice[]) bondedDevices.toArray();
+                    BluetoothDevice device = devices[0];
+                    ParcelUuid[] uuids = device.getUuids();
+                    BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
+                    socket.connect();
+                    outputStream = socket.getOutputStream();
+                    inStream = socket.getInputStream();
+                }
+
+                Log.e("error", "No appropriate paired devices.");
+            }else{
+                Log.e("error", "Bluetooth is disabled.");
             }
         }
-
-        
     }
 
+    public void write(String s) throws IOException {
+        outputStream.write(s.getBytes());
+    }
 
+    public void run() {
+        final int BUFFER_SIZE = 1024;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytes = 0;
+        int b = BUFFER_SIZE;
+
+        while (true) {
+            try {
+                bytes = inStream.read(buffer, bytes, BUFFER_SIZE - bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
